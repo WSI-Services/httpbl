77912d5314ea04505ded47d55dfbe239bf72c9a1
Sam Likins <sam.likins@wsi-services.com>
2014-04-21 12:53:52 -0400

    Changed path to composer.

--- utilities/travis/before-script.sh
+++ utilities/travis/before-script.sh
@@ -2,8 +2,7 @@
 
 set -ev
 
-COMPOSER_PATH=$(whereis composer)
-COMPOSER_PATH="/${COMPOSER_PATH#*/}"
+COMPOSER_PATH="~/.phpenv/versions/$(phpenv version-name)/bin/composer.phar"
 
 sudo $COMPOSER_PATH self-update
 $COMPOSER_PATH install --dev
